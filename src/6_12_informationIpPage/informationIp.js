1cbb393b39584a993bbe43691a76feb811bf3425
Mika <mikberza@mail.ru>
2023-05-01 19:50:02 +0300

    Проект 1.1_multicalculators_JavaScript Исправлены ошибки в файлах (informationIp.js) в соответствии с инструкциями ESLint
    Project 1.1_multicalculators_JavaScript
    Fixed file errors (informationIp.js), according to the instructions of ESLint

--- src/6_12_informationIpPage/informationIp.js
+++ src/6_12_informationIpPage/informationIp.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';  отключил строгий режим по требованию eslint
 // импортируем стили css
 import '../6_13_globalFiles/headerAndFooterCustomStyle.css';
 import './informationIp.css';
@@ -13,33 +13,38 @@ const arrTableName = document.querySelectorAll('.content__table-name');
 const arrTableMeaning = document.querySelectorAll('.content__table-meaning');
 
 const arrTableNameError = document.querySelectorAll(
-  '.content__table-name-error'
+  '.content__table-name-error',
 );
 const arrTableMeaningError = document.querySelectorAll(
-  '.content__table-meaning-error'
+  '.content__table-meaning-error',
 );
 
 // API для получения своего IP
+// eslint-disable-next-line camelcase
 const url_IP = 'https://api.ipify.org?format=json';
 // API для получения информации об IP (вместо-(161.185.160.93) подставить свой/чужой IP)
+
+// eslint-disable-next-line camelcase, quotes, no-unused-vars
 const url_data_IP = `http://ip-api.com/json/161.185.160.93`;
 
-/*Глобальный fetch()метод запускает процесс извлечения ресурса из сети, 
-возвращая обещание (promise), которое выполняется, как только ответ 
+/* Глобальный fetch()метод запускает процесс извлечения ресурса из сети,
+возвращая обещание (promise), которое выполняется, как только ответ
 становится доступным. */
 
-/*async - У этого слова один простой смысл: эта функция всегда возвращает промис. 
-Значения других типов оборачиваются в завершившийся успешно 
-промис автоматически.*/
+/* async - У этого слова один простой смысл: эта функция всегда возвращает промис.
+Значения других типов оборачиваются в завершившийся успешно
+промис автоматически. */
 
-/*await - ключевое слово  заставит интерпретатор JavaScript ждать до тех пор, пока 
-промис справа от await не выполнится. После чего оно вернёт его результат, 
+/* await - ключевое слово  заставит интерпретатор JavaScript ждать до тех пор, пока
+промис справа от await не выполнится. После чего оно вернёт его результат,
 и выполнение кода продолжится. */
 
 // async await - асинхронное ожидание
 
 // Функция для получения текущего IP адреса
+// eslint-disable-next-line consistent-return
 async function getCurrentIPAddress(urlIP) {
+  let result;
   try {
     // метод fetch возвращая обещание (promise)
     // ответ от сервера c текущим IP адресом
@@ -47,27 +52,32 @@ async function getCurrentIPAddress(urlIP) {
     // response.json() – декодирует ответ в формате JSON
     // тело ответа от сервера c текущим IP адресом
     const responseBodyCurrentIP = await responseWithTheCurrentIP.json();
-    return responseBodyCurrentIP;
+    result = responseBodyCurrentIP;
+    return result;
   } catch (error) {
-    console.log(error);
+    result = error;
+    return result;
   }
 }
 
 // Функция для получения данных IP-адреса
 async function getDataIP(ip) {
+  let result;
   try {
     // метод fetch возвращая обещание (promise)
     // ответ от сервера c текущими данными по IP-адресу
     const responseWithCurrentIPData = await fetch(
-      `http://ip-api.com/json/${ip}`
+      `http://ip-api.com/json/${ip}`,
     );
     // response.json() – декодирует ответ в формате JSON
     // тело ответа от сервера c текущими данными по IP-адресу
     const responseBodyCurrentIPData = responseWithCurrentIPData.json();
     // console.log(responseBodyCurrentIPData);
-    return responseBodyCurrentIPData;
+    result = responseBodyCurrentIPData;
+    return result;
   } catch (error) {
-    console.log(error);
+    result = error;
+    return result;
   }
 }
 
@@ -84,7 +94,7 @@ function checkTheResponseCode(status) {
   }
   if (status >= 400 && status <= 499) {
     responseCode = `код ответа ${status} - (ошибка клиента)`;
-    console.log(`код ответа ${status} - (ошибка клиента)`);
+    // console.log(`код ответа ${status} - (ошибка клиента)`);
   }
   if (status >= 500 && status <= 526) {
     responseCode = `код ответа ${status} - (ошибка сервера)`;
@@ -125,25 +135,25 @@ function outputTheTableWithAnError(arrKeys, arrValues) {
     arrTableNameError[1].textContent = `${arrKeys[1]} : `;
 
     arrTableMeaningError[0].textContent = checkTheResponseCode(arrValues[0]);
-    let textError = `${arrValues[1].title}. ${arrValues[1].message}.`;
+    const textError = `${arrValues[1].title}. ${arrValues[1].message}.`;
     arrTableMeaningError[1].textContent = textError;
   }
 }
 
-getCurrentIPAddress(url_IP).then(function (ipAddress) {
-  console.log(ipAddress.ip);
+getCurrentIPAddress(url_IP).then((ipAddress) => {
+  // console.log(ipAddress.ip);
   // по клику вывожу в инпут текущий IP-адрес
-  btnCurrent.addEventListener('click', function () {
+  btnCurrent.addEventListener('click', () => {
     inpCurrent.value = ipAddress.ip;
   });
 });
 
-btnData.addEventListener('click', function () {
-  getDataIP(inpData.value).then(function (ipData) {
+btnData.addEventListener('click', () => {
+  getDataIP(inpData.value).then((ipData) => {
     // массив с ключами объекта
-    let arrKeysIpData = Object.keys(ipData);
+    const arrKeysIpData = Object.keys(ipData);
     // массив с значениями объекта
-    let arrValueIpData = Object.values(ipData);
+    const arrValueIpData = Object.values(ipData);
 
     // Функция выводит ответ от сервера в таблицу (таблица БЕЗ ОШИБОК)
     outputTheTableWithoutErrors(arrKeysIpData, arrValueIpData);
