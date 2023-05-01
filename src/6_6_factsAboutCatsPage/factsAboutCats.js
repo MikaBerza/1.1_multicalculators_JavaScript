355ed268f98c5cebbc4d6605c334231836ac288d
Mika <mikberza@mail.ru>
2023-05-01 19:00:29 +0300

    Проект 1.1_multicalculators_JavaScript Исправлены ошибки в файлах (factsAboutCats.js) в соответствии с инструкциями ESLint
    Project 1.1_multicalculators_JavaScript
    Fixed file errors (factsAboutCats.js), according to the instructions of ESLint

--- src/6_6_factsAboutCatsPage/factsAboutCats.js
+++ src/6_6_factsAboutCatsPage/factsAboutCats.js
@@ -1,10 +1,11 @@
-/*активация строго режима*/
-'use strict';
+// 'use strict';  отключил строгий режим по требованию eslint
 // импортируем стили css
 import '../6_13_globalFiles/headerAndFooterCustomStyle.css';
 import './factsAboutCats.css';
 // импортируем изображения
+// eslint-disable-next-line no-unused-vars
 import defaultCat from './imgFactsAboutCats/1.41defaultCat.jpg';
+// eslint-disable-next-line no-unused-vars
 import loadingError from './imgFactsAboutCats/1.42loadingError.jpg';
 //
 //
@@ -18,23 +19,23 @@ const img = document.querySelector('.card__img');
 const urlCatFact = 'https://catfact.ninja/fact';
 // console.log(urlCatFact);
 
-/*Глобальный fetch()метод запускает процесс извлечения ресурса из сети, 
-возвращая обещание (promise), которое выполняется, как только ответ 
+/* Глобальный fetch()метод запускает процесс извлечения ресурса из сети,
+возвращая обещание (promise), которое выполняется, как только ответ
 становится доступным. */
 
-/*async - У этого слова один простой смысл: эта функция всегда возвращает промис. 
-Значения других типов оборачиваются в завершившийся успешно 
-промис автоматически.*/
+/* async - У этого слова один простой смысл: эта функция всегда возвращает промис.
+Значения других типов оборачиваются в завершившийся успешно
+промис автоматически. */
 
-/*await - ключевое слово  заставит интерпретатор JavaScript ждать до тех пор, пока 
-промис справа от await не выполнится. После чего оно вернёт его результат, 
+/* await - ключевое слово  заставит интерпретатор JavaScript ждать до тех пор, пока
+промис справа от await не выполнится. После чего оно вернёт его результат,
 и выполнение кода продолжится. */
 
 // async await - асинхронное ожидание
 async function fetchHandler() {
   try {
-    /*здесь храниться ответ от сервера, если бы не была написано 
-    конструкция async await, то мы бы получали промис.*/
+    /* здесь храниться ответ от сервера, если бы не была написано
+    конструкция async await, то мы бы получали промис */
     const responseFact = await fetch(urlCatFact);
     // console.log(responseFact);
 
@@ -46,7 +47,7 @@ async function fetchHandler() {
   } catch (error) {
     // если возникнет ошибка, то мы выведем текст (Ошибка загрузки !!!)
     fact.textContent = 'Ошибка загрузки !!!';
-    console.log(error);
+    // console.log(error);
   }
 }
 
